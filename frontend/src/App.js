cd70dcc09885ed910803a65d427f61f5e3cd1d0d
Jake Garcia <bballwiz4@gmail.com>
2020-10-06 19:45:55 -0700

    Home component added and rows and columns set up for title alignment.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Home from './components/Home/Home';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,12 @@ class App extends Component {
   render() {
     return (
       <div className="App container">
-        <h1>Hello World</h1>
+        <div className="row">
+          <div className="col-12">
+          <h1>Hello World</h1>
+          </div>
+        </div>
+        <Home />
       </div>
     )
   }
