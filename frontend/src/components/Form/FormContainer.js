ea10c37e53008d71f3558f67f80458617b886add
github username <bballwiz4@gmail.com>
2020-10-27 16:09:52 -0700

    Team name select dropdown sstarted and the position drop down filled out with options.

--- frontend/src/components/Form/FormContainer.js
+++ frontend/src/components/Form/FormContainer.js
@@ -3,11 +3,19 @@ import './FormContainer.css';
 
 class FormContainer extends Component {
 
+    state = {
+        playerPositions: ["QB", "RB", "WR", "TE", "DEF", "K"]
+    }
+
     // componentDidMount() {
     //     // More To Come
     //     // Load up all the weekly injuries using sportsradar
     //     // To check if your player is on the list
     // }
+
+    renderPositionOptions = () => {
+        return this.state.playerPositions.map(position => <option value={position}>{position}</option>)
+    }
 
 
     render() {
@@ -17,11 +25,17 @@ class FormContainer extends Component {
                 <form>
                     <div className="form-group row">
                         <div className="col-6">
-                            <label forHTML="playerName">Player Name</label>
-                            <input className="form-control" type="text" name="playerName" id="playerName" />
+                            <label forHTML="teamName">Team City</label>
+                            <select className="form-control" name="playerName" id="playerName" >
+                                <option>LV</option>
+                                <option>SEA</option>
+                            </select>
                         </div>
                         <div className="col-6">
-
+                            <label forHTML="position">Player Position</label>
+                            <select className="form-control" id="position" name="position">
+                                {this.renderPositionOptions()}
+                            </select>
                         </div>
                     </div>
                 </form>
